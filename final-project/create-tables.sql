53bb3a4e355d29db751e9ff66c0a0fe35b9aab8e
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2024-05-25 11:00:55 +0300

    Add files via upload

---
+++ final-project/create-tables.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE [Bulgaria Demographics]
+
+USE [Bulgaria Demographics]
+
+CREATE TABLE Years(
+	Id SMALLINT PRIMARY KEY IDENTITY(1950, 1)
+	,[Year] SMALLINT NOT NULL
+);
+
+CREATE TABLE [Population](
+	Id INT PRIMARY KEY IDENTITY
+	,[YearId] SMALLINT FOREIGN KEY REFERENCES Years(Id) NOT NULL
+	,[Population] INT 
+	,[Births] INT
+	,[Deaths] INT
+	,[Net Migration] INT
+	,[Growth Rate (%)] DECIMAL (4, 2)
+	,[Life Expectancy] DECIMAL (4, 2)
+	,[Total Fertility Rate] DECIMAL(4, 2)
+	,[Net Reproduction Rate] DECIMAL (3, 2)
+	,[Infant Mortality Rate] DECIMAL (4, 2)
+);
